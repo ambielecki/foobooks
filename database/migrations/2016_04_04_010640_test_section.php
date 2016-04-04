2c84406d784dcade5bd52a1f67b12f99b9937425
Andrew Bielecki <ambielecki@gmail.com>
2016-04-03 21:19:22 -0400

    added migration to git silly

---
+++ database/migrations/2016_04_04_010640_test_section.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TestSection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function ($table) {
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function ($table) {
+            $table->dropColumn('email');
+        });
+    }
+}
